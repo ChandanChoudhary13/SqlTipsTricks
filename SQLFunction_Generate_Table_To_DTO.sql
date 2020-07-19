f6119e5a6384840c4f8b548d81e8c8369914f4b9
ChandanChoudhary13 <65276032+ChandanChoudhary13@users.noreply.github.com>
2020-07-19 15:38:48 +0530

    SQL Function To Generate C# DTO | Class | Model From SQL Table

---
+++ SQLFunction_Generate_Table_To_DTO.sql
@@ -0,0 +1,69 @@
+
+-- =============================================
+-- Author:      Chandan Choudhary
+-- Create Date: 19 July 2020
+-- Description: To Generate DTO or c# Model from Table.
+
+-- Test Block
+-- SELECT  dbo.GenerateTableToModel('dbo','CustomerType','CustomerTypeDTO')
+-- =============================================
+CREATE FUNCTION GenerateTableToModel
+(
+	--------------- Input arguments ---------------
+	@schemaName NVARCHAR(50), 
+	@tableName NVARCHAR(50), 
+	@className NVARCHAR(50)
+	--------------- Input arguments ---------------
+)
+RETURNS varchar(MAX) -- or whatever length you need
+AS
+BEGIN
+    Declare @ResponseText VARCHAR(MAX);
+
+	DECLARE tableColumns CURSOR LOCAL FOR
+	select 
+	-- schema_name(tab.schema_id) as schema_name, tab.name as table_name, col.column_id, col.name as column_name, t.name as data_type, col.max_length, col.precision
+	col.name, col.system_type_id, col.is_nullable
+	from sys.tables as tab
+	inner join sys.columns as col on tab.object_id = col.object_id
+	left join sys.types as t on col.user_type_id = t.user_type_id
+	Where schema_name(tab.schema_id) =@schemaName
+	and  tab.name=@tableName
+
+	SET @ResponseText = 'public class ' + @className
+	SET @ResponseText += '{'
+ 
+	OPEN tableColumns
+	DECLARE @name NVARCHAR(MAX), @typeId INT, @isNullable BIT, @typeName NVARCHAR(MAX)
+	FETCH NEXT FROM tableColumns INTO @name, @typeId, @isNullable
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		SET @typeName =
+		CASE @typeId
+			WHEN 36 THEN 'Guid'
+			WHEN 56 THEN 'int'
+			WHEN 127 THEN 'long'
+			WHEN 59 THEN 'decimal'
+			WHEN 61 THEN 'DateTime'
+			WHEN 104 THEN 'bool'
+			WHEN 106 THEN 'double'
+			WHEN 231 THEN 'string'
+			WHEN 239 THEN 'string'
+			WHEN 167 THEN 'string'
+			WHEN 241 THEN 'XElement'
+			ELSE 'TODO(' + CAST(@typeId AS NVARCHAR) + ')'
+		END;
+		IF @isNullable = 1 AND @typeId != 231 AND @typeId != 239 AND @typeId != 241
+			SET @typeName = @typeName + (CASE WHEN @typeName='string' THEN '' ELSE '?' END)
+		SET @ResponseText += '    public ' + @typeName + ' ' + @name + ' { get; set; }'	
+		FETCH NEXT FROM tableColumns INTO @name, @typeId, @isNullable
+	END
+ 
+	SET @ResponseText += '}'
+ 
+	CLOSE tableColumns
+
+    RETURN  @ResponseText
+
+END
+GO
